a4975089b4f5207c45260d25bb9a6138b2bc6103
naishac <79550754+naishac@users.noreply.github.com>
2021-07-14 12:36:43 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return(
+      <Text style={{marginTop: 50}}>My first App</Text>
+    );
+  }
+}
